8dd8021c434df504e8d4f3978a3c73b6d341dacf
Computroniks <mnickson@sidingsmedia.sytes.net>
2020-08-10 23:41:53 +0100

    Added copyright at top

--- source/desktop client/js/render.js
+++ source/desktop client/js/render.js
@@ -1,6 +1,10 @@
+// ./js/render.js
+//Copyright 2020 Sidings Media
+//License: MIT
 const customTitlebar = require('custom-electron-titlebar');
+const bgcolour = '#323233';
 let titlebar = new customTitlebar.Titlebar({
-	backgroundColor: customTitlebar.Color.fromHex('#323233'),
+	backgroundColor: customTitlebar.Color.fromHex(bgcolour),
 	icon: './assets/logos/logo.png',
 });
 
