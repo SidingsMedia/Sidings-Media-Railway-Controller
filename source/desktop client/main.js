4e652c8fe6035e4701446cba27fa839940336405
Computroniks <mnickson@sidingsmedia.sytes.net>
2020-08-11 22:42:20 +0100

    Removed bootstrap and created basic css design

--- source/desktop client/main.js
+++ source/desktop client/main.js
@@ -28,6 +28,8 @@ function createWindow (){
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        minWidth:800,
+        minHeight:600,
         icon: 'assets/logos/logo.png',
         backgroundColor: '#323233',
         webPreferences: {
