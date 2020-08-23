7486fc7cf633e8f438cc860acac11606266800bf
Computroniks <mnickson@sidingsmedia.sytes.net>
2020-08-23 22:42:24 +0100

    Added functionality to add control pannels
    Currnetly the changes are not saved but this is to be added
    soon.

--- source/desktop client/js/render.js
+++ source/desktop client/js/render.js
@@ -1,12 +1,25 @@
 // ./js/render.js
 //Copyright 2020 Sidings Media
 //License: MIT
+var script = document.createElement('script'); 
+ 
+script.src = './node_modules/jquery/dist/jquery.min.js'; 
+document.getElementsByTagName('head')[0].appendChild(script);
 const customTitlebar = require('custom-electron-titlebar');
 const bgcolour = '#323233';
 let titlebar = new customTitlebar.Titlebar({
 	backgroundColor: customTitlebar.Color.fromHex(bgcolour),
 	icon: './assets/logos/logo.png',
 });
+function addControl() {
+	var txt1 = `
+		<div class=\"control\">
+			<h1>This is a control area</h1>
+		</div>
+	`;        
+	$("#main").append(txt1);        
+
+}
 
 
 
