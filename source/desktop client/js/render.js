fa116169c9b4069048c8366d58a7d902454f15fd
Computroniks <mnickson@sidingsmedia.sytes.net>
2020-08-25 15:20:30 +0100

    Added comments

--- source/desktop client/js/render.js
+++ source/desktop client/js/render.js
@@ -1,10 +1,12 @@
 // ./js/render.js
 //Copyright 2020 Sidings Media
 //License: MIT
+
+//Include JQuery
 var script = document.createElement('script'); 
- 
 script.src = './node_modules/jquery/dist/jquery.min.js'; 
 document.getElementsByTagName('head')[0].appendChild(script);
+
 const customTitlebar = require('custom-electron-titlebar');
 const bgcolour = '#323233';
 let titlebar = new customTitlebar.Titlebar({
