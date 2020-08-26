041b02b34f3a488f3327f7ffd37d5d94fec67154
Computroniks <mnickson@sidingsmedia.sytes.net>
2020-08-26 23:50:10 +0100

    Added more urls to dictionary

--- source/desktop client/js/openUrl.js
+++ source/desktop client/js/openUrl.js
@@ -1,3 +1,6 @@
+// ./js/openUrl.js
+//Copyright 2020 Sidings Media
+//License: MIT
 const{ shell } = require('electron')
 var urls = {
     facebook: 'https://www.facebook.com/Sidings-Media-589191041769930',
@@ -6,7 +9,13 @@ var urls = {
     instagram: 'https://www.instagram.com/sidingsmedia/',
     tumblr: 'https://sidingsmedia.tumblr.com',
     github: 'https://github.com/sidingsmedia',
-    easyeda: 'https://easyeda.com/Sidings-Media/'
+    easyeda: 'https://easyeda.com/Sidings-Media/',
+    docs: 'https://smrcdocs.sidingsmedia.com',
+    license: 'https://raw.githubusercontent.com/SidingsMedia/DC-Model-Railway-Controller/master/LICENSE',
+    contribute: 'https://github.com/SidingsMedia/Sidings-Media-Railway-Controller/blob/master/CONTRIBUTING.md',
+    website: 'https://sidingsmedia.com',
+    thesidings: 'https://www.youtube.com/channel/UCZmBfzy3_Jdo5YrsE6o04sg',
+    sidingselectric: 'https://www.youtube.com/channel/UCZOIvXgjCMrqjUHvgx_213A'
 }
 function openURL(platform){
     shell.openExternal(urls[platform])
