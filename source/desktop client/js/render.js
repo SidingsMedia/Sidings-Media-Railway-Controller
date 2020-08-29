ac621ec1d14adaa8132e980396a17f670b891344
Computroniks <mnickson@sidingsmedia.sytes.net>
2020-08-29 19:42:22 +0100

    Added settings button to control area pannel

--- source/desktop client/js/render.js
+++ source/desktop client/js/render.js
@@ -22,6 +22,25 @@ function addControl() {
 	$("#main").append(html);        
 
 }
+/* When the user clicks on the button, 
+            toggle between hiding and showing the dropdown content */
+function showSettingsDropdown(id) {
+	document.getElementById(id).classList.toggle("show");
+	}
+	
+	// Close the dropdown if the user clicks outside of it
+	window.onclick = function(event) {
+	if (!event.target.matches('.settingsDropdownBtn')) {
+		var dropdowns = document.getElementsByClassName("settingsDropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+		var openDropdown = dropdowns[i];
+		if (openDropdown.classList.contains('show')) {
+			openDropdown.classList.remove('show');
+		}
+		}
+	}
+	}
 
 
 
