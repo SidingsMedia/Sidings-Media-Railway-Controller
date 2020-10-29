03b5e6c382141cec5a12f67d327f97475a3e5577
Computroniks <mnickson@sidingsmedia.sytes.net>
2020-10-29 18:40:44 +0000

    Added popup for setting board details

--- source/desktop client/js/render.js
+++ source/desktop client/js/render.js
@@ -71,7 +71,32 @@ function showSettingsDropdown(id) {
 		}
 		}
 	}
-	}
+    }
+// Get the modal
+var modal = document.getElementById("add-control-popup");
+
+// Get the button that opens the modal
+var btn = document.getElementById("add-control");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("popup-close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 
