3d294b510326d451031c635e5928e300c50e4165
Computroniks <mnickson@sidingsmedia.sytes.net>
2020-10-29 21:06:14 +0000

    Moved pannel controls to controlPanels.js

--- source/desktop client/js/render.js
+++ source/desktop client/js/render.js
@@ -1,103 +1,17 @@
 // ./js/render.js
 //Copyright 2020 Sidings Media
 //License: MIT
-var Mousetrap = require('mousetrap');
 
-//Include JQuery
-var script = document.createElement('script'); 
-script.src = './node_modules/jquery/dist/jquery.min.js'; 
-document.getElementsByTagName('head')[0].appendChild(script);
+//Include required modules
+const Mousetrap = require('mousetrap');
 const customTitlebar = require('custom-electron-titlebar');
+
+//Set titlebar
 const bgcolour = '#323233';
 let titlebar = new customTitlebar.Titlebar({
 	backgroundColor: customTitlebar.Color.fromHex(bgcolour),
 	icon: './assets/logos/logo.png',
 });
-function addControl() {
-	var html = `
-				<div class="control">
-                    <div class="control-main">
-                        <h2>Relay Board 1</h2>
-                        <table class="control-main-buttons">
-                            <tr>
-                                <td><button>Channel 1</button></td>
-                                <td><button>Channel 2</button></td>
-                                <td><button>Channel 3</button></td>
-                                <td><button>Channel 4</button></td>
-                            </tr>
-                            <tr>
-                                <td><button>Channel 5</button></td>
-                                <td><button>Channel 6</button></td>
-                                <td><button>Channel 7</button></td>
-                                <td><button>Channel 8</button></td>
-                            </tr>
-                            <tr>
-                                <td><button>Channel 9</button></td>
-                                <td><button>Channel 10</button></td>
-                                <td><button>Channel 11</button></td>
-                                <td><button>Channel 12</button></td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="control-sidebar">
-                        <div class="settingsDropdown">
-                            <button onclick="showSettingsDropdown('settingsDropdown')" class="settingsDropdownBtn control-sidebar-button"><i class="fas fa-ellipsis-h"></i></button>
-                            <div id="settingsDropdown" class="settingsDropdown-content">
-							  <button class="control-sidebar-button"><i class="fas fa-trash-alt"></i></button>
-							  <button class="control-sidebar-button"><i class="fas fa-pen"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-	`;        
-	$("#main").append(html);        
-
-}
-/* When the user clicks on the button, 
-            toggle between hiding and showing the dropdown content */
-function showSettingsDropdown(id) {
-	document.getElementById(id).classList.toggle("show");
-	}
-	
-	// Close the dropdown if the user clicks outside of it
-	window.onclick = function(event) {
-	if (!event.target.matches('.settingsDropdownBtn')) {
-		var dropdowns = document.getElementsByClassName("settingsDropdown-content");
-		var i;
-		for (i = 0; i < dropdowns.length; i++) {
-		var openDropdown = dropdowns[i];
-		if (openDropdown.classList.contains('show')) {
-			openDropdown.classList.remove('show');
-		}
-		}
-	}
-    }
-// Get the modal
-var modal = document.getElementById("add-control-popup");
-
-// Get the button that opens the modal
-var btn = document.getElementById("add-control");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("popup-close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
 
 
 
@@ -123,3 +37,4 @@ Mousetrap.bind('mod+shift+i', function() {
 Mousetrap.bind('mod+m', function() {
 	sendRequest({window: 'mainWindow', func:'min'})
 })
+
