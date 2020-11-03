0c23589fa171ba58e4af0a46a5ffff10aba8a484
Computroniks <mnickson@sidingsmedia.sytes.net>
2020-11-03 12:08:01 +0000

    Created files

---
+++ source/firmware/main board/mainboard.ino
@@ -0,0 +1,4 @@
+// ./source/firmware/main board/mainboard.ino
+//Copyright (c) 2020 Sidings Media, All rights reserved
+//License: MIT
+//See LICENSE in the project root for license information.
---
+++ source/firmware/relay board/relayboard.ino
@@ -0,0 +1,5 @@
+// ./source/firmware/relay board/relayboard.ino
+//Copyright (c) 2020 Sidings Media, All rights reserved
+//License: MIT
+//See LICENSE in the project root for license information.
+
