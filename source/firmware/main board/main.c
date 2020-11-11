d4d15da970f78f56688915ef6ba89e2ed7d69945
Computroniks <mnickson@sidingsmedia.sytes.net>
2020-11-11 20:38:41 +0000

    Initial commit

---
+++ source/firmware/main board/main.c
@@ -0,0 +1,25 @@
+/*Copyright (c) 2020 Sidings Media, All rights reserved
+ *You may use, distribute and modify this code under the
+ *terms of the MIT license which can be found in the 
+ *project root.
+ *
+ *You should have received a copy of the MIT license with
+ *this file. If not, please write to: 
+ *support@sidingsmedia.com, or visit : 
+ *https://raw.githubusercontent.com/SidingsMedia/Sidings-Media-Railway-Controller/master/LICENSE
+ *
+ *Name: main.c
+ *Project: SMRC - firmware - main board
+ */ 
+
+#define F_CPU 16000000UL //Set clock speed to 16MHz
+//Include necessary header files
+#include <avr/io.h>
+#include <util/delay.h>
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
