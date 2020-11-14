45136adcc19dfa903c84068eae3a45f1054c02e8
Computroniks <mnickson@sidingsmedia.sytes.net>
2020-11-14 19:29:53 +0000

    Basic usart functionality

--- source/firmware/10relay/main.c
+++ source/firmware/10relay/main.c
@@ -13,14 +13,44 @@
  */ 
 
 #define F_CPU 16000000UL //Set clock speed to 16MHz
+#define UBRR 103//(F_CPU/(16*9600))-1 //Calculate baud rate
 //Include necessary header files
 #include <avr/io.h>
 #include <util/delay.h>
+void serialOut(char ch)
+{
+	while ((UCSR0A & (1<<UDRE0))==0);
+	UDR0 = ch;
+}
+char serialIn()
+{
+	while ( !(UCSR0A & (1 << RXC0)));
+	
+	return UDR0;
+}
 int main(void)
 {
-    /* Replace with your application code */
+    DDRD |= 0xFE; //Set pins pd2 - pd7 to output
+	DDRC |= 0x3C; //Set pins pc2 - pc5 to output
+	UBRR0 = UBRR; //Set baud rate
+	UCSR0B |= (1 << RXEN0); //Turn on receiver
+	UCSR0B|= (1<< TXEN0); //Turn on transmitter
+	UCSR0C |= (3 << UCSZ00); //Set async operation, no parity, one stop bit, 8 data bits
+	int channels[10] = {PORTD2, PORTD3, PORTD4, PORTD5, PORTD6, PORTD7, PORTC5, PORTC4, PORTC3, PORTC2};
     while (1) 
+		
     {
+		int input = serialIn() -'0';
+		//int inputInt = input - '0';
+		switch(input){
+			case 0 ... 5://Channel 0 off
+				PORTD ^= (1 << channels[input]);
+				break;
+			case 6 ... 9:
+				PORTC ^= (1 << channels[input]);
+				break;
+			default:break;
+		}
     }
 }
 
