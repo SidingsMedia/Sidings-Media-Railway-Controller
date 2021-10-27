7fe207ce4cbb3bcf35ad21a568a3549d1661c0c5
Matthew Nickson <mnickson@sidingsmedia.com>
2021-10-27 22:27:09 +0100

    Put logo paths in right place
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

--- docs/conf.py
+++ docs/conf.py
@@ -92,11 +92,13 @@ html_theme_options = {
     'navigation_depth': 3,
     'includehidden': True,
     'titles_only': False,
-    'html_logo': 'https://raw.githubusercontent.com/SidingsMedia/SMRC-logos/main/rendered/track-bw-square-192.pngs',
-    'html_favicon': 'https://raw.githubusercontent.com/SidingsMedia/SMRC-logos/main/rendered/favicon.ico'
 }
 
 html_theme_path = ["_themes", ]
+
+
+html_logo = './_static/logos/rendered/track-bw-square-192.png',
+html_favicon = './_static/logos/rendered/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
