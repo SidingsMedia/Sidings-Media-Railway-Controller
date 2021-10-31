862cd310c40ff706c76730e5c4d10138317556cf
Matthew Nickson <mnickson@sidingsmedia.com>
2021-10-31 20:28:04 +0000

    Updated messages
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

--- docs/_static/stable-warning.js
+++ docs/_static/stable-warning.js
@@ -10,14 +10,14 @@ function warnUnstable() {
   }else if (window.READTHEDOCS_DATA.version === 'develop'){
 	warning.innerHTML = "<p class='first admonition-title'>Note</p> " +
 		"<p class='last'> " +
-		"This documentation is unstable and is prone to change at any time." +
-		"Documentation is available for the <a href='/projects/smrc/en/stable/'>current stable release</a>, " +
+		"This documentation is a development version and as such it is unstable and is prone to change at any time. " +
+		"Documentation is available for the <a href='/projects/smrc/en/stable/'>current stable release</a> "
 		+ "</p>";
   } else{
 	warning.innerHTML = "<p class='first admonition-title'>Note</p> " +
 	"<p class='last'> " +
-	"This document is for the latest version. This documentation may be unstable and may change." +
-	"Documentation is available for the <a href='/projects/smrc/en/stable/'>current stable release</a>, " +
+	"This document is for the latest version and is in the process of being finalized. This documentation may be unstable and may change. " +
+	"Documentation is available for the <a href='/projects/smrc/en/stable/'>current stable release</a> and previous versions can be selected by using the" +
 	"</p>";
   }
 
