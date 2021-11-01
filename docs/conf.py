621745f9bd2aeaf8667f0d854cd306a11c0aad4e
Matthew Nickson <mnickson@sidingsmedia.com>
2021-11-01 23:13:25 +0000

    Added extra line before warning
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

--- docs/conf.py
+++ docs/conf.py
@@ -222,7 +222,7 @@ latex_elements = {
 		}}
     ''',
 	'maketitle': f'''
-	\\newcommand\\sphinxbackoftitlepage{{{{This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.\\\\{warning}}}}}\\sphinxmaketitle
+	\\newcommand\\sphinxbackoftitlepage{{{{This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.\\\\\\\\{warning}}}}}\\sphinxmaketitle
 	'''
     # Latex figure (float) alignment
     #
